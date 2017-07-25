b2c60af761cddd7b1445b5f1586503abafb37324
Jose <josepanta63@gmail.com>
2017-07-25 17:27:51 -0500

    Agregado clases para el proceso evolutivo

--- Horario_1.0/src/horario_1/pkg0/mainPrueba.java
+++ Horario_1.0/src/horario_1/pkg0/mainPrueba.java
@@ -1,5 +1,21 @@
 package horario_1.pkg0;
 
+import java.util.ArrayList;
+
 public class mainPrueba {
-    
+
+    public static void main(String arg[]) {
+        int idSeccion[]={};
+        int idDocente[]={};
+        int idSala[]={};
+        int diaSemana[]={};
+        int Hora[]={};
+        int cuadroCromosoma[][]={{1,1,7,23,1,1},{1,1,7,23,1,2},{1,1,7,23,1,3},{1,1,7,23,1,4},
+                                {1,2,45,12,2,7},{1,2,45,12,2,8},{1,3,6,45,5,12},{1,3,6,45,5,13},
+                                {1,3,6,45,5,14},{1,4,3,5,4,5},{1,4,3,5,4,6},{1,4,3,5,1,1}};
+        cromosomaIn croIn = new cromosomaIn();
+        for (int i = 0; i < 8; i++) {
+            
+        }
+    }
 }
