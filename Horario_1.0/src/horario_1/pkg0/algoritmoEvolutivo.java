fc1c2d3f45df30d9a8b1a97aa183a89382d11d18
Jose <josepanta63@gmail.com>
2017-07-25 21:03:01 -0500

    Copiando clases del algoritmo evolutivo

---
+++ Horario_1.0/src/horario_1/pkg0/algoritmoEvolutivo.java
@@ -0,0 +1,23 @@
+package horario_1.pkg0;
+
+/**
+ *
+ * @author Jose
+ */
+public class algoritmoEvolutivo {
+    
+    public algoritmoEvolutivo() {
+
+    }
+    public void evolucionPoblacion(){
+        
+    }
+
+    public void crucePadres() {
+
+    }
+
+    public void mutacionIndividuo() {
+
+    }
+}
---
+++ Horario_1.0/src/horario_1/pkg0/cromosomaIn.java
@@ -0,0 +1,16 @@
+package horario_1.pkg0;
+
+import java.util.ArrayList;
+
+public class cromosomaIn {
+
+    ArrayList<Integer> idSeccion=new ArrayList<>();
+    ArrayList<Integer> idDocente=new ArrayList<>();
+    ArrayList<Integer> idSala=new ArrayList<>();
+    ArrayList<Integer> diaSemana=new ArrayList<>();
+    ArrayList<Integer> Hora=new ArrayList<>();
+
+    public cromosomaIn() {
+        
+    }
+}
